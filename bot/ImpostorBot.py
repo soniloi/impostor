9f796bf05b1b229918dbd5d611f26cd2d9a9f508
soniloi <niloinso@tcd.ie>
2017-05-06 21:14:02 +0100

    Split Mystery out of bot

--- bot/ImpostorBot.py
+++ bot/ImpostorBot.py
@@ -5,6 +5,8 @@ import random
 import re
 
 import Config
+from Mystery import HintType
+from Mystery import Mystery
 from Players import PlayerCollection
 from Players import PlayerScoreType
 
@@ -23,6 +25,9 @@ from twisted.python import log
 
 # system imports
 import time, sys
+
+
+StatisticFormat = namedtuple("StatisticFormat", "function, container")
 
 
 class Style:
@@ -50,73 +55,6 @@ class Colour:
   LIGHT_GREY = "15"
 
 
-class HintType:
-  NICK_CHARACTER = 0
-  ADDITIONAL_QUOTE = 1
-
-
-class Mystery:
-
-  def __init__(self, ident, author, author_aliases, initial_quote, hints):
-    self.ident = ident
-    self.author = author
-    self.author_aliases = author_aliases
-    self.quotes = [initial_quote]
-    self.nick_characters = []
-    self.future_hints = hints
-    self.already_guessed = set()
-
-  def describe(self):
-
-    description = "The mystery author says: [" + self.quotes[0] + "]"
-    for quote in self.quotes[1:]:
-      description += " and [" + quote + "]"
-    description += ". "
-
-    if self.nick_characters:
-      description += "Their nick contains the character(s) ["
-      description += ",".join(self.nick_characters)
-      description += "]. "
-
-    if self.already_guessed:
-      description += "The nick(s) guessed so far are ["
-      description += ",".join(self.already_guessed)
-      description += "]. "
-
-    return description
-
-  def getHint(self):
-
-    hint_message = ""
-
-    if not self.future_hints:
-      hint_message = "I have no further hints to give. "
-
-    else:
-      hint_index = random.randint(0, len(self.future_hints) - 1)
-      (hint_type, hint) = self.future_hints[hint_index]
-      del self.future_hints[hint_index]
-
-      if hint_type == HintType.NICK_CHARACTER:
-        self.nick_characters.append(hint)
-        hint_message = "The mystery author's name contains the character [" + hint + "]"
-
-      elif hint_type == HintType.ADDITIONAL_QUOTE:
-        self.quotes.append(hint)
-        hint_message = "The mystery author also says: [" + hint + "]"
-
-    return hint_message
-
-  # Evaluate a guess, with aliasing
-  # Return the original (real) nick if correct, or None if incorrect
-  def guess(self, guess):
-    self.already_guessed.add(guess)
-    if guess == self.author or guess in self.author_aliases:
-      return self.author
-    return None
-
-
-
 class MessageLogger:
   """
   An independent logger class (because separation of application
@@ -133,9 +71,6 @@ class MessageLogger:
 
   def close(self):
     self.file.close()
-
-
-StatisticFormat = namedtuple("StatisticFormat", "function, container")
 
 
 class ImpostorBot(irc.IRCClient):
---
+++ bot/Mystery.py
@@ -0,0 +1,68 @@
+import random
+
+
+class HintType:
+  NICK_CHARACTER = 0
+  ADDITIONAL_QUOTE = 1
+
+
+class Mystery:
+
+  def __init__(self, ident, author, author_aliases, initial_quote, hints):
+    self.ident = ident
+    self.author = author
+    self.author_aliases = author_aliases
+    self.quotes = [initial_quote]
+    self.nick_characters = []
+    self.future_hints = hints
+    self.already_guessed = set()
+
+  def describe(self):
+
+    description = "The mystery author says: [" + self.quotes[0] + "]"
+    for quote in self.quotes[1:]:
+      description += " and [" + quote + "]"
+    description += ". "
+
+    if self.nick_characters:
+      description += "Their nick contains the character(s) ["
+      description += ",".join(self.nick_characters)
+      description += "]. "
+
+    if self.already_guessed:
+      description += "The nick(s) guessed so far are ["
+      description += ",".join(self.already_guessed)
+      description += "]. "
+
+    return description
+
+  def getHint(self):
+
+    hint_message = ""
+
+    if not self.future_hints:
+      hint_message = "I have no further hints to give. "
+
+    else:
+      hint_index = random.randint(0, len(self.future_hints) - 1)
+      (hint_type, hint) = self.future_hints[hint_index]
+      del self.future_hints[hint_index]
+
+      if hint_type == HintType.NICK_CHARACTER:
+        self.nick_characters.append(hint)
+        hint_message = "The mystery author's name contains the character [" + hint + "]"
+
+      elif hint_type == HintType.ADDITIONAL_QUOTE:
+        self.quotes.append(hint)
+        hint_message = "The mystery author also says: [" + hint + "]"
+
+    return hint_message
+
+  # Evaluate a guess, with aliasing
+  # Return the original (real) nick if correct, or None if incorrect
+  def guess(self, guess):
+    self.already_guessed.add(guess)
+    if guess == self.author or guess in self.author_aliases:
+      return self.author
+    return None
+
