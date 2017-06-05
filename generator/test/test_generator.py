18fc98c38d08ea3a5f110eac8b68b07b221bc0e5
soniloi <niloinso@tcd.ie>
2017-06-05 16:55:02 +0100

    Fixed incorrect use of mock

--- generator/test/test_generator.py
+++ generator/test/test_generator.py
@@ -1,25 +1,29 @@
 import mock
+from mock import patch
 import unittest
 
 from .. import generator
-
+from .. import users
 
 class TestGenerator(unittest.TestCase):
 
   def setUp(self):
     pass
 
+
   def test_init_empty(self):
 
-    mock_users = mock.Mock()
-    mock_meta = {}
-    mock_time = 0
+    with patch(users.__name__ + ".UserCollection") as users_mock:
 
-    local_generator = generator.Generator()
-    local_generator.init(mock_users, mock_meta, mock_time)
+      users_instance = users_mock.return_value
+      users_instance.empty.return_value = True
 
-    self.assertTrue(local_generator.empty())
-    mock_users.empty.assert_called_once_with()
+      mock_meta = {}
+      mock_time = 0
+
+      local_generator = generator.Generator()
+      local_generator.init(users_instance, mock_meta, mock_time)
+      self.assertTrue(local_generator.empty())
 
 
 if __name__ == "__main__":
