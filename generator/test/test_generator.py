ae3c1ce0001465722c16795b1d6f824be4ab8c47
soniloi <niloinso@tcd.ie>
2017-06-11 13:07:24 +0100

    Corrected and completed tests for getGenericStatistics

--- generator/test/test_generator.py
+++ generator/test/test_generator.py
@@ -2,6 +2,7 @@ import mock
 from mock import patch
 import unittest
 
+from .. import config
 from .. import generator
 from .. import users
 
@@ -30,15 +31,16 @@ class TestGenerator(unittest.TestCase):
 
     meta = {}
     time = 0
+    user_count = 0
 
     with patch(users.__name__ + ".UserCollection") as users_mock:
 
       users_instance = users_mock.return_value
-      users_instance.countUsers.return_value = 0
+      users_instance.countUsers.return_value = user_count
       users_instance.getBiggestUsers.return_value = None
       users_instance.getMostQuoted.return_value = None
 
-      local_generator.init(users_mock, meta, time)
+      local_generator.init(users_instance, meta, time)
       stats = local_generator.getGenericStatistics()
 
       self.assertTrue(generator.GenericStatisticType.USER_COUNT in stats)
@@ -48,13 +50,69 @@ class TestGenerator(unittest.TestCase):
       self.assertTrue(generator.GenericStatisticType.BIGGEST_USERS in stats)
       self.assertTrue(generator.GenericStatisticType.MOST_QUOTED_USERS in stats)
 
+      self.assertEqual(stats[generator.GenericStatisticType.USER_COUNT], user_count)
       self.assertEqual(stats[generator.GenericStatisticType.DATE_STARTED], time)
       self.assertEqual(stats[generator.GenericStatisticType.DATE_GENERATED], None)
 
       source_channels = stats[generator.GenericStatisticType.SOURCE_CHANNELS]
-      self.assertFalse(source_channels.primary)
+      self.assertEqual(source_channels.primary, None)
       self.assertFalse(source_channels.additionals)
 
+      self.assertEqual(stats[generator.GenericStatisticType.BIGGEST_USERS], None)
+      self.assertEqual(stats[generator.GenericStatisticType.MOST_QUOTED_USERS], None)
+
+
+  def test_get_generic_statistics_nonempty(self):
+
+    local_generator = generator.Generator()
+
+    primary = "#ocean"
+    additional = ["#pond", "#sea", "#lake"]
+
+    meta = {
+      config.META_PRIMARY: [primary],
+      config.META_ADDITIONAL: additional,
+    }
+
+    time = 97
+    user_count = 3
+
+    user_nick_1 = "ailm"
+    user_nick_2 = "beith"
+    user_nick_3 = "coll"
+
+    big_users = [
+      users.NickAndCount(user_nick_1, 79),
+      users.NickAndCount(user_nick_1, 23),
+      users.NickAndCount(user_nick_1, 11),
+    ]
+
+    quoted_users = [
+      users.NickAndCount(user_nick_3, 346),
+      users.NickAndCount(user_nick_1, 129),
+      users.NickAndCount(user_nick_2, 62),
+    ]
+
+    with patch(users.__name__ + ".UserCollection") as users_mock:
+
+      users_instance = users_mock.return_value
+      users_instance.countUsers.return_value = user_count
+      users_instance.getBiggestUsers.return_value = big_users
+      users_instance.getMostQuoted.return_value = quoted_users
+
+      local_generator.init(users_instance, meta, time)
+      stats = local_generator.getGenericStatistics()
+
+      self.assertEqual(stats[generator.GenericStatisticType.USER_COUNT], user_count)
+      self.assertEqual(stats[generator.GenericStatisticType.DATE_STARTED], time)
+      self.assertEqual(stats[generator.GenericStatisticType.DATE_GENERATED], None)
+
+      source_channels = stats[generator.GenericStatisticType.SOURCE_CHANNELS]
+      self.assertEqual(source_channels.primary, primary)
+      self.assertEqual(source_channels.additionals, tuple(additional))
+
+      self.assertEqual(stats[generator.GenericStatisticType.BIGGEST_USERS], big_users)
+      self.assertEqual(stats[generator.GenericStatisticType.MOST_QUOTED_USERS], quoted_users)
 
 if __name__ == "__main__":
   unittest.main()
