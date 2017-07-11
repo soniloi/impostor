b56d73bc573a04284d46daa40e434df31ad054eb
soniloi <niloinso@tcd.ie>
2017-07-11 10:25:14 +0100

    Added testing for processor help

--- bot/test/test_processor.py
+++ bot/test/test_processor.py
@@ -31,6 +31,59 @@ class TestProcessor(unittest.TestCase):
       self.assertTrue("?" in proc.commands)
 
 
+  def test_trigger_meta_help_generic(self):
+
+    with patch(generator.__name__ + ".Generator") as generator_mock, \
+          patch(players.__name__ + ".PlayerCollection") as players_mock:
+
+      generator_instance = generator_mock.return_value
+      generator_instance.empty.return_value = False
+
+      players_instance = players_mock.return_value
+
+      proc = processor.RequestProcessor("", generator_instance, players_instance)
+
+      response = proc.triggerMeta("mollusc", "@help")
+
+      self.assertEquals(len(response), 1)
+      self.assertEquals(response[0], processor.RequestProcessor.BOT_DESC_BASIC)
+
+
+  def test_trigger_meta_help_specific_unknown(self):
+
+    with patch(generator.__name__ + ".Generator") as generator_mock, \
+          patch(players.__name__ + ".PlayerCollection") as players_mock:
+
+      generator_instance = generator_mock.return_value
+      generator_instance.empty.return_value = False
+
+      players_instance = players_mock.return_value
+
+      proc = processor.RequestProcessor("", generator_instance, players_instance)
+
+      response = proc.triggerMeta("mollusc", "@help nonsense")
+
+      self.assertEquals(len(response), 0)
+
+
+  def test_trigger_meta_help_specific_known(self):
+
+    with patch(generator.__name__ + ".Generator") as generator_mock, \
+          patch(players.__name__ + ".PlayerCollection") as players_mock:
+
+      generator_instance = generator_mock.return_value
+      generator_instance.empty.return_value = False
+
+      players_instance = players_mock.return_value
+
+      proc = processor.RequestProcessor("", generator_instance, players_instance)
+
+      response = proc.triggerMeta("mollusc", "@help mystery")
+
+      self.assertEquals(len(response), 1)
+      self.assertEquals(response[0], processor.RequestProcessor.HELP_MYSTERY)
+
+
 if __name__ == "__main__":
   unittest.main()
 
