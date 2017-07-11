f503a12c000c742538d9a4d306287bda09c01f82
soniloi <niloinso@tcd.ie>
2017-07-11 10:36:03 +0100

    Tidied up common unit test code

--- bot/test/test_processor.py
+++ bot/test/test_processor.py
@@ -12,76 +12,44 @@ class TestProcessor(unittest.TestCase):
 
   def setUp(self):
 
-    pass
+    with patch(generator.__name__ + ".Generator") as generator_mock, \
+          patch(players.__name__ + ".PlayerCollection") as players_mock:
+
+      generator_instance = generator_mock.return_value
+      generator_instance.empty.return_value = False
+
+      players_instance = players_mock.return_value
+
+      self.processor = processor.RequestProcessor("", generator_instance, players_instance)
 
 
   def test_init(self):
 
-    with patch(generator.__name__ + ".Generator") as generator_mock, \
-          patch(players.__name__ + ".PlayerCollection") as players_mock:
-
-      generator_instance = generator_mock.return_value
-      generator_instance.empty.return_value = False
-
-      players_instance = players_mock.return_value
-
-      proc = processor.RequestProcessor("", generator_instance, players_instance)
-
-      self.assertTrue("stats" in proc.commands)
-      self.assertTrue("?" in proc.commands)
+    self.assertTrue("stats" in self.processor.commands)
+    self.assertTrue("?" in self.processor.commands)
 
 
   def test_trigger_meta_help_generic(self):
 
-    with patch(generator.__name__ + ".Generator") as generator_mock, \
-          patch(players.__name__ + ".PlayerCollection") as players_mock:
+    response = self.processor.triggerMeta("mollusc", "@help")
 
-      generator_instance = generator_mock.return_value
-      generator_instance.empty.return_value = False
-
-      players_instance = players_mock.return_value
-
-      proc = processor.RequestProcessor("", generator_instance, players_instance)
-
-      response = proc.triggerMeta("mollusc", "@help")
-
-      self.assertEquals(len(response), 1)
-      self.assertEquals(response[0], processor.RequestProcessor.BOT_DESC_BASIC)
+    self.assertEquals(len(response), 1)
+    self.assertEquals(response[0], processor.RequestProcessor.BOT_DESC_BASIC)
 
 
   def test_trigger_meta_help_specific_unknown(self):
 
-    with patch(generator.__name__ + ".Generator") as generator_mock, \
-          patch(players.__name__ + ".PlayerCollection") as players_mock:
+    response = self.processor.triggerMeta("mollusc", "@help nonsense")
 
-      generator_instance = generator_mock.return_value
-      generator_instance.empty.return_value = False
-
-      players_instance = players_mock.return_value
-
-      proc = processor.RequestProcessor("", generator_instance, players_instance)
-
-      response = proc.triggerMeta("mollusc", "@help nonsense")
-
-      self.assertEquals(len(response), 0)
+    self.assertEquals(len(response), 0)
 
 
   def test_trigger_meta_help_specific_known(self):
 
-    with patch(generator.__name__ + ".Generator") as generator_mock, \
-          patch(players.__name__ + ".PlayerCollection") as players_mock:
+    response = self.processor.triggerMeta("mollusc", "@help mystery")
 
-      generator_instance = generator_mock.return_value
-      generator_instance.empty.return_value = False
-
-      players_instance = players_mock.return_value
-
-      proc = processor.RequestProcessor("", generator_instance, players_instance)
-
-      response = proc.triggerMeta("mollusc", "@help mystery")
-
-      self.assertEquals(len(response), 1)
-      self.assertEquals(response[0], processor.RequestProcessor.HELP_MYSTERY)
+    self.assertEquals(len(response), 1)
+    self.assertEquals(response[0], processor.RequestProcessor.HELP_MYSTERY)
 
 
 if __name__ == "__main__":
